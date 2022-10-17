01c70dada2eb2e01820d1241e2656ca23d5c080d
Houre Souren <112374244+houresouren81@users.noreply.github.com>
2022-10-17 22:50:45 +0200

    försöker hålla udernamen inloggat

--- script.js
+++ script.js
@@ -4,6 +4,26 @@ const loggain = document.getElementById("loggain");
 const loggaut = document.getElementById("loggaut");
 
 
+
+
+
+
+
+
+if (localStorage.getItem("username") == "Sara" && localStorage.getItem("password")=="qwe123") { 
+
+    document.getElementById("sida1").style.display= "none";
+    document.getElementById("loggaut").style.display="block";
+
+    
+   
+   
+    
+    }
+
+
+
+
 loggain.addEventListener("click", function () {
 
     if (username.value == "Sara" && password.value == "qwe123") {
@@ -11,30 +31,31 @@ loggain.addEventListener("click", function () {
         localStorage.setItem("usernamn", "Sara");
         localStorage.setItem("password", "qwe123");
         document.getElementById("sida1").style.display = "none";
-        
-    
+
+
         document.getElementById("loggaut").style.display = "block";
+
+
     }
     else {
 
         alert("Fel password försök igen.");
-    }});
+    }
+});
+
+
+loggaut.addEventListener("click", function () {
 
 
 
+    localStorage.removeItem("usernamn", "Sara");
+    localStorage.removeItem("password", "qwe123");
+    document.getElementById("sida1").style.display = "block";
 
-    loggaut.addEventListener("click", function () {
+    document.getElementById("loggaut").style.display = "none";
+}
 
 
-
-        localStorage.removeItem("usernamn", "Sara");
-        localStorage.removeItem("password", "qwe123");
-        document.getElementById("sida1").style.display = "block";
-
-        document.getElementById("loggaut").style.display = "none";
-    } 
-  
-    
 );
 
 
